58ec3eb808de9d5ed753f5b084ac0eb392b7b85b
Lucas Florio <lucasefe@gmail.com>
2023-07-03 23:31:52 -0300

    play kick on 1 and hh on 2, 3 and 4

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,37 +1,46 @@
 import useSound from 'use-sound';
 import { useInterval } from 'usehooks-ts'
-import snare from './samples/snare.wav';
+import hihat from './samples/HH2.wav';
+import kick from './samples/BD2.wav';
 import React from 'react';
 import { PlayCircle } from 'react-feather';
 import { PauseCircle } from 'react-feather';
 
 
-
-const PlayButton = () => {
-  const [play] = useSound(snare);
-
-  return <button onClick={play}>Boom!</button>;
-};
-
-
 const defaultBPM = 80;
 function Metronome() {
+  const [playHiHat] = useSound(hihat);
+  const [playKick] = useSound(kick);
   const [isPlaying, setPlaying] = React.useState(false);
   const [bpm, setBPM] = React.useState(defaultBPM);
-  const [play] = useSound(snare);
+  const [count, setCount] = React.useState(0);
 
   useInterval(() => {
-    play()
+    setCount(count + 1)
+    const shouldPlayKick = (count % 4) === 0;
+    if (shouldPlayKick)
+      playKick();
+    else
+      playHiHat();
   },
     isPlaying ? (1000 * 60 / bpm) : null
   )
 
-  const label = isPlaying ? <PauseCircle size="100" /> : <PlayCircle size="100" />;
+
+  const handleClick = () => {
+    if (isPlaying)
+      setCount(0);
+
+    setPlaying(!isPlaying)
+  }
+
+  const aText = isPlaying ? <PauseCircle size="100" /> : <PlayCircle size="100" />;
 
   return <div>
     <h3>Metronome</h3>
     <h4>{bpm} BPM</h4>
-    <input type="range"
+    <input
+      type="range"
       onChange={(e) => setBPM(e.target.value)}
       value={bpm}
       min={60}
@@ -39,9 +48,8 @@ function Metronome() {
       step={1}
     />
     <br />
-    <a onClick={() => setPlaying(!isPlaying)}>{label}
-    </a>
-  </div>
+    <a onClick={handleClick}>{aText}</a>
+  </div >
 }
 
 
